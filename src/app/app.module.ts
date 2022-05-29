62ea0bed73274acbdd550927f62f41ea9594fbee
Doc1998 <docmp98@gmail.com>
2022-05-29 11:49:48 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingComponent } from './landing/landing.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LandingComponent
+    LandingComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
