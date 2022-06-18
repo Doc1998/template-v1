1d8d941774c280558718c832a267219e03d97205
Doc1998 <docmp98@gmail.com>
2022-06-18 14:07:37 +0100

    articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutUsComponent } from "./about-us/about-us.component";
+import { ArticlesComponent } from "./articles/articles.component";
 import { ContactComponent } from "./contact/contact.component";
 import { LandingComponent } from "./landing/landing.component";
 import { ProjectsComponent } from "./projects/projects.component";
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: "about-us", component: AboutUsComponent, data: {title : 'About us | Small business Web Development | Ireland Web Designs'} },
   { path: "reviews", component: ReviewsComponent, data: {title : 'Reviews | Small business Web Development | Ireland Web Designs'}},
   { path: "projects", component: ProjectsComponent,  data: {title : 'Projects | Small business Web Development | Ireland Web Designs'}},
-  { path: "contact", component: ContactComponent, data: {title : 'Contact | Small business Web Development | Ireland Web Designs'}}
+  { path: "contact", component: ContactComponent, data: {title : 'Contact | Small business Web Development | Ireland Web Designs'}},
+  { path: "articles", component: ArticlesComponent, data: {title : 'Articles | Small business Web Development | Ireland Web Designs'}}
 ];
 
 @NgModule({
