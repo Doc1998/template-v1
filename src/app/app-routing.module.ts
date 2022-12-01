5420465adcf2b12fcd7ee7d900fee881cd6e65b0
Donal <docmp98@gmail.com>
2022-12-01 14:07:39 +0000

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutUsComponent } from "./about-us/about-us.component";
 import { BookingComponent } from "./booking/booking.component";
 import { ContactComponent } from "./contact/contact.component";
 import { LandingComponent } from "./landing/landing.component";
+import { PaymentsComponent } from "./payments/payments.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ReviewsComponent } from "./reviews/reviews.component";
 import { ServicesComponent } from "./services/services.component";
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: "", component: LandingComponent },
   { path: "contact", component: ContactComponent },
   { path: "services", component: ServicesComponent },
+  { path: "payments", component: PaymentsComponent },
 
 
 ];
